4540d110c5c5340ac12d5f834e5ff933ea4c3921
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-07-29 21:15:04 -0600

    Create extra-packages.sh

---
+++ extra-packages.sh
@@ -0,0 +1,11 @@
+## Extra Pacakges Install
+sudo xbps-install -S --yes alacritty fff fzf gnupg gtk+3-devel mercurial olm python3-pip zathura zip zsh
+
+## Alacritty Config
+cd
+mkdir ~/.config/alacritty/
+cd ~/void-deploy/configs
+cp alacritty.yml ~/.config/alacritty/
+cd
+
+## WIP
