e42c5d0a8d9e2b905c4c0641703a17333557dc58
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-07-30 19:34:31 -0600

    Create .set_monitor.sh

---
+++ scripts/.set_monitor.sh
@@ -0,0 +1,5 @@
+#1/bin/bash
+
+xrandr --newmode "1600x900_165.00" 364.35  1600 1736 1912 2224  900 903 908 994
+xrandr --addmode Virtual 1600x900_165.00
+xrandr --output Virtual1 --mode 1600x900_165.00
