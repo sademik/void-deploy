d1482974818746eddcc2c344e7663078fee60fd5
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-09-20 20:39:05 -0600

    Update void-deploy.sh

--- void-deploy.sh
+++ void-deploy.sh
@@ -1,17 +1,22 @@
 # Void Linux with DWM Deployment (my flavour)
 
+
 ## System Update
-echo "Updating the system."
 sudo xbps-install -Syy
 sudo xbps-install -u xbps
 sudo xbps-install -Syu
 
+
+## Enable Non-Free/Multilib Repos
+sudo xbps-install -S --yes void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
+sudo xbps-install -Syy
+
+
 ## Install Packages
-echo "Installing some packages."
-sudo xbps-install -S --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop mpv feh terminus-font nerd-fonts-ttf exa neofetch vim alacritty fff fzf cmus gnupg gtk+3-devel p7zip mercurial olm python3-pip zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip zsh xz binutils tar ffmpeg
+sudo xbps-install -S --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop btop mpv feh terminus-font nerd-fonts-ttf exa neofetch vim alacritty fff fzf cmus gnupg gtk+3-devel p7zip mercurial olm python3-pip zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip zsh xz binutils tar ffmpeg bluez mdadm bridge-utils cryptsetup element-desktop dejavu-fonts-ttf dmidecode font-misc-misc github-cli glxinfo gvfs-afc gvfs-mtp gvfs-smb libcurl-devel libdrm-32bit libgcc-32bit libopenvc-python3 libstdc++-32bit libvirt lvm2 mesa-dri-32bit mesa-vaapi mesa-vdpau mesa-vulkan-radeon mono ncdu network-manager-applet noto-fonts-cjk noto-fonts-emoji ntfs-3g olm olm-python3 openbsd-netcat pfetch python-devel python3-devel python3-distutils-extra qemu steam udisks2 virt-manager virt-viewer vkd3d vkd3d-devel w3m w3m-img weechat weechat-python wmctrl xarchiver xauth xorg-input-drivers xorg-minimal xorg-video-drivers xsel void-docs-browse rust cargo ufetch
+
 
 ## Setting up DWM
-echo "Setting up DWM."
 mkdir suckless
 cd suckless
 ## Eventually thesee will be changed to my own repos with my self-configured versions of the Suckless tools.
@@ -36,6 +41,7 @@ make
 sudo make clean install
 cd
 
+
 ##Constructing .xinitrc
 touch ~/.xinitrc
 echo "setxkbmap us &" >> .xinitrc
@@ -48,27 +54,36 @@ echo "feh --bg-fill ~/void-deploy/wallpapers/blame1.jpg" >> .xinitrc
 echo "~/void-deploy/scripts/.set_time.sh" >> .xinitrc
 echo "wait $wmpid" >> .xinitrc
 
+
 ## Make .set_monitor.sh and .set_time.sh executable
 sudo chmod +x ~/void-deploy/scripts/.set_monitor.sh
 sudo chmod +x ~/void-deploy/scripts/.set_time.sh
 
+
 ## Make .config
 mkdir ~/.config
 
-## Alacritty configuration
+
+## Alacritty Configuration
 cd
 mkdir ~/.config/alacritty/
-cd ~/void-deploy/configs
-cp alacritty.yml ~/.config/alacritty/
+cp ~/void-deploy/configs/alacritty.yml ~/.config/alacritty/
+
+
+## VIM Configuration
+cp ~/void-deploy/configs/.vimrc .vimrc
+sudo curl https://raw.githubusercontent.com/stayradiated/dotfiles/master/apps/vim/colors/shblah.vim --output /usr/share/vim/vim90/colors/shblah.vim
 cd
 
-## ZSH Config (test)
-sudo chsh -s /bin/zsh faen
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-cd
-cp ~/void-deploy/configs/zsh/.zshrc ~/.zshrc
-cp ~/void-deploy/configs/zsh/.p10k.zsh ~/.p10k.zsh
+
+## ZSH Config
+#sudo chsh -s /bin/zsh faen
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#cd
+#cp ~/void-deploy/configs/zsh/.zshrc ~/.zshrc
+#cp ~/void-deploy/configs/zsh/.p10k.zsh ~/.p10k.zsh
+
 
 ## Copy fonts
 mkdir ~/.fonts
@@ -77,12 +92,11 @@ cp ~/void-deploy/fonts/MesloLGS\ NF\ Bold\ Italic.ttf ~/.fonts
 cp ~/void-deploy/fonts/MesloLGS\ NF\ Italic.ttf ~/.fonts
 cp ~/void-deploy/fonts/MesloLGS\ NF\ Regular.ttf ~/.fonts
 
+
 ## Zathura configuration
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 mkdir ~/.config/zathura/
 cp ~/void-deploy/configs/zathurarc ~/.config/zathura/zathurarc
 
-## Run 'startx' from tty
-echo "Run 'startx' from tty"
 
 sudo reboot
