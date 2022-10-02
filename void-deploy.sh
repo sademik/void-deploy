793b1c14e717e5897fd203712cfc342a77691f5a
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-10-02 13:14:29 -0600

    Update void-deploy.sh

--- void-deploy.sh
+++ void-deploy.sh
@@ -9,25 +9,25 @@ sudo xbps-install -Syu
 
 
 ## Enable Non-Free/Multilib Repos
-sudo xbps-install -S -v --yes void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
+sudo xbps-install -S --yes void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 sudo xbps-install -Syy
 
 
 ## Define Minimal Installation Packages Function
 minimal-install () {
-sudo xbps-install -S -v --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop btop mpv feh exa neofetch vim alacritty fzf cmus gnupg gtk+3-devel p7zip mercurial zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip xz binutils ffmpeg ufetch w3m w3m-img xsel wally-cli setxkbmap xbindkeys task ntfs-3g
+sudo xbps-install -S --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop btop mpv feh exa neofetch vim alacritty fzf cmus gnupg gtk+3-devel p7zip mercurial zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip xz binutils ffmpeg ufetch w3m w3m-img xsel wally-cli setxkbmap xbindkeys task ntfs-3g
 }
 
 
 ## Define Full Installation Packages Function
 full-install () {
-sudo xbps-install -S -v --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop btop mpv feh terminus-font nerd-fonts-ttf exa neofetch vim alacritty fff fzf cmus gnupg gtk+3-devel p7zip mercurial python3-pip zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip xz binutils ffmpeg bluez mdadm bridge-utils cryptsetup element-desktop dejavu-fonts-ttf dmidecode font-misc-misc github-cli glxinfo gvfs-afc gvfs-mtp gvfs-smb libcurl-devel libdrm-32bit libgcc-32bit libopencv-python3 libstdc++-32bit libvirt lvm2 mesa-dri-32bit mesa-vaapi mesa-vdpau mesa-vulkan-radeon mono ncdu network-manager-applet noto-fonts-cjk noto-fonts-emoji ntfs-3g olm olm-python3 openbsd-netcat pfetch python-devel python3-devel python3-distutils-extra qemu steam udisks2 virt-manager virt-viewer vkd3d vkd3d-devel w3m w3m-img weechat weechat-python wmctrl xarchiver xauth xorg-input-drivers xorg-minimal xorg-video-drivers xsel void-docs-browse rust cargo ufetch rxvt-unicode SDL SDL-32bit SDL-devel SDL-devel-32bit SDL_image-devel SDL_image SDL_ttf ImageMagick dhclient exiftool glu gimp fuse-overlayfs libglvnd-32bit moc python3-argh python3-ConfigArgParse python3-distlib python-distutils-extra wally-cli tree setxkbmap task xbindkeys idle-python3
+sudo xbps-install -S --yes base-devel xorg libXft-devel libX11-devel libXinerama-devel libXt-devel libcurl-devel dbus-devel dbus-glib-devel curl wget xtools ranger xdg-desktop-portal pulseaudio pulseaudio-devel ntp micro pcmanfm firefox nodejs htop btop mpv feh terminus-font nerd-fonts-ttf exa neofetch vim alacritty fff fzf cmus gnupg gtk+3-devel p7zip mercurial python3-pip zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zip xz binutils ffmpeg bluez mdadm bridge-utils cryptsetup element-desktop dejavu-fonts-ttf dmidecode font-misc-misc github-cli glxinfo gvfs-afc gvfs-mtp gvfs-smb libcurl-devel libdrm-32bit libgcc-32bit libopencv-python3 libstdc++-32bit libvirt lvm2 mesa-dri-32bit mesa-vaapi mesa-vdpau mesa-vulkan-radeon mono ncdu network-manager-applet noto-fonts-cjk noto-fonts-emoji ntfs-3g olm olm-python3 openbsd-netcat pfetch python-devel python3-devel python3-distutils-extra qemu steam udisks2 virt-manager virt-viewer vkd3d vkd3d-devel w3m w3m-img weechat weechat-python wmctrl xarchiver xauth xorg-input-drivers xorg-minimal xorg-video-drivers xsel void-docs-browse rust cargo ufetch rxvt-unicode SDL SDL-32bit SDL-devel SDL-devel-32bit SDL_image-devel SDL_image SDL_ttf ImageMagick dhclient exiftool glu gimp fuse-overlayfs libglvnd-32bit moc python3-argh python3-ConfigArgParse python3-distlib python-distutils-extra wally-cli tree setxkbmap task xbindkeys idle-python3
 }
 
 
 ## Define ZSH Installation and Configuration Function
 zsh-install () {
-sudo xbps-install -S -v --yes zsh
+sudo xbps-install -S --yes zsh
 sudo chsh -s /bin/zsh faen
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -39,7 +39,7 @@ cp ~/void-deploy/configs/zsh/.p10k.zsh ~/.p10k.zsh
 
 ## Define FISH Installation and Configuration Function
 fish-install () {
-sudo xbps-install -S -v --yes fish-shell
+sudo xbps-install -S --yes fish-shell
 sudo echo /bin/fish | sudo tee -a /etc/shells
 sudo chsh -s /bin/fish faen
 }
@@ -127,6 +127,13 @@ sudo make clean install
 cd
 
 
+## SOWM Setup
+git clone https://github.com/sademik/sowm
+cd sowm
+sudo make sowm
+cd
+
+
 ## Constructing .xinitrc files (temporary, will eventually come with repo)
 touch ~/.xinitrc_dwm
 echo 'if [ -d /etc/X11/xinit/xinitrc.d ] ; then' >> .xinitrc_dwm 
@@ -199,4 +206,19 @@ mkdir ~/.config/zathura/
 cp ~/void-deploy/configs/zathurarc ~/.config/zathura/zathurarc
 
 
-sudo reboot
+## User chooses whether or not to reboot
+PS3='Reboot? '
+options=("Yes" "No")
+select opt in "${options[@]}"
+do
+  case $opt in
+    "Yes")
+      sudo reboot
+      break
+      ;;
+    "No")
+      break
+      ;;
+    *) echo "Invalid option: $REPLY";;
+  esac
+done
