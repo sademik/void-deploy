e8321dd02d9238557217fb5924efa71a08dfa75d
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-10-04 21:31:16 -0600

    Add files via upload

---
+++ scripts/lemonbar.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+Clock(){
+    DATETIME=$(date "+%a %-d %b, %-l:%M %p")
+    echo -e -n "${DATETIME}"
+}
+
+User(){
+    echo -n $USER@$(hostname)
+}
+
+Uptime(){
+    m=$(uptime -p)
+    echo -n ${m:2}
+}
+
+Cpu(){
+    c=$(top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1}')
+    line="    "
+    printf "${line:${#c}}""%s %s" $c%
+
+
+}
+
+Memory(){
+     mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+     memp=${mem:0:-2}%
+     line="       "
+     printf "${line:${#memp}}""%s %s" $memp
+}
+ 
+while true; do
+	echo -e "$(User) [$(Uptime)] [$(Cpu)] []$(Memory)] [$(Clock)]"
+	#Uncomment the sleep command here if $(Network) isn't active otherwise the sleep command is provided in it
+	sleep 1s
+done
