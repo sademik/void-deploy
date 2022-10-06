4cebea23924d0ff7702fc933dd5413be584fafc7
säde mikkonen <79290067+sademik@users.noreply.github.com>
2022-10-05 22:07:09 -0600

    Update lemonbar.sh

--- scripts/lemonbar.sh
+++ scripts/lemonbar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Clock(){
-    DATETIME=$(date "+%a %-d %b, %-l:%M %p")
+    DATETIME=$(date "+%a %-d %b - %-l:%M %p")
     echo -e -n "${DATETIME}"
 }
 
@@ -32,7 +32,44 @@ Memory(){
 }
  
 while true; do
-	echo -e "[$(User)] [$(Uptime)] [$(Cpu)] [$(Memory)] [$(Clock)]"
+	echo -e "%{l}%{F#CCCCCC}%{B#0B0B0B} [$(Cpu)] [$(Memory)]%{c}[ $(User) ] %{r}[ $(Clock) ] "
 	#Uncomment the sleep command here if $(Network) isn't active otherwise the sleep command is provided in it
+	sleep 1s
+done
+#!/bin/bash
+
+Clock(){
+    DATETIME=$(date "+%a %-d %b - %-l:%M %p")
+    echo -e -n "${DATETIME}"
+}
+
+User(){
+    echo -n $USER@$(hostname)
+}
+
+Uptime(){
+    m=$(uptime -p)
+    echo -n ${m:2}
+}
+
+Cpu(){
+    c=$(top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1}')
+    line="    "
+    printf "${line:${#c}}""%s %s" $c%
+
+
+}
+
+Memory(){
+     mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+     memp=${mem:0:-2}%
+     line="       "
+     printf "${line:${#memp}}""%s %s" $memp
+}
+ 
+while true; do
+	echo -e "%{l}%{F#CCCCCC}%{B#0B0B0B} [$(Cpu)] [$(Memory)]%{c}[ $(User) ] %{r}[ $(Clock) ] "
 	sleep 1s
 done
